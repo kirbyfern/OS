511fc09838ae1a5708b3523bb10770bd0e4f5e32
kirbygit <kirbyjames.fernandez01@student.csulb.edu>
2023-09-16 10:52:51 -0700

    Added comments for each line of code

--- child.cpp
+++ child.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <string>
+#include <iostream>  // Include necessary header for input and output operations.
+#include <string>     // Include necessary header for working with strings.
 
-using namespace std;
+using namespace std; // Use the standard C++ namespace.
 
-int main(int argc, char* argv[]) {
-    if(argc != 3) {
-      cerr << "Usage: " << argv[0] << " child_number name_of_child" << endl;
-
-      return 1;
+// Main function with command-line arguments.
+int main(int argc, char* argv[]) { // argc holds the no. of command line arguments, argv stores the integer of the command line arguments into a vector
+    if(argc != 3) { // Check if the number of command-line arguments is not 3.
+      cerr << "Usage: " << argv[0] << " child_number name_of_child" << endl; // Print an error message with usage instructions.
+      return 1; // Exit the program with an error code.
     }
 
-    int childNumber = stoi(argv[1]);
-    string childName = argv[2];
+    int childNumber = stoi(argv[1]); // Convert the first argument to an integer and store it as childNumber.
+    string childName = argv[2];      // Store the second argument as childName.
 
-    cout << "I am child number " << childNumber << ", and my name is " << childName << "." << endl;
+    cout << "I am child number " << childNumber << ", and my name is " << childName << "." << endl; // Display the child's number and name.
 
-    return 0;
+    return 0; // Exit the program with a success code.
 }
--- parent.cpp
+++ parent.cpp
@@ -1,51 +1,53 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
+#include <iostream>        // Include necessary header for input and output operations.
+#include <string>          // Include necessary header for working with strings.
+#include <vector>          // Include necessary header for using vectors.
+#include <sys/types.h>     // Include necessary header for system types.
+#include <sys/wait.h>      // Include necessary header for wait functions.
+#include <unistd.h>        // Include necessary header for system calls.
 
-using namespace std;
+using namespace std;       // Use the standard C++ namespace.
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " name_1 name_2 ... name_k" << endl;
-        return 1;
+int main(int argc, char* argv[]) {  // Main function with command-line arguments.
+    if (argc < 2) {                // Check if there are not enough command-line arguments.
+        cerr << "Usage: " << argv[0] << " name_1 name_2 ... name_k" << endl;  // Print an error message with usage instructions.
+        return 1;                   // Exit the program with an error code.
     }
 
-    int numChildren = argc - 1;
-    vector<string> childNames;
+    int numChildren = argc - 1;    // Calculate the number of child processes.
+    vector<string> childNames;     // Create a vector to store child names.
 
     // Store child names in a vector
     for (int i = 1; i < argc; ++i) {
-        childNames.push_back(argv[i]);
+        childNames.push_back(argv[i]);  // Add each child name to the vector.
     }
 
-    cout << "I have " << numChildren << " children." << endl;
+    cout << "I have " << numChildren << " children." << endl;  // Display the number of children.
 
     // Create child processes
     for (int i = 0; i < numChildren; ++i) {
-        pid_t pid = fork();
+        pid_t pid = fork();  // Create a child process.
 
         if (pid == -1) {
-            cerr << "Fork failed." << endl;
-            return 1;
+            cerr << "Fork failed." << endl;  // If fork fails, print an error message.
+            return 1;                         // Exit the program with an error code.
         } else if (pid == 0) {
             // Child process
             execl("./child", "./child", to_string(i + 1).c_str(), childNames[i].c_str(), nullptr);
+            // Execute the child program with appropriate arguments.
+
             // If execl() fails
-            cerr << "Exec failed for child process " << i + 1 << endl;
-            return 1;
+            cerr << "Exec failed for child process " << i + 1 << endl;  // Print an error message.
+            return 1;  // Exit the child process with an error code.
         }
     }
 
     // Wait for all child processes to terminate
     for (int i = 0; i < numChildren; ++i) {
         int status;
-        wait(&status);
+        wait(&status);  // Wait for a child process to finish.
     }
 
-    cout << "All child processes terminated. Parent exits." << endl;
+    cout << "All child processes terminated. Parent exits." << endl;  // Display a message when all children are done.
 
-    return 0;
+    return 0;  // Exit the program with a success code.
 }
